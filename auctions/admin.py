0a43c1c80b932f944f9c4e8867bb657447cea036
Ali <ali.zolghadr@gmail.com>
2024-01-16 19:35:42 +1100

    Add models for Listings, Comments, and Bids

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from auctions.models import User
+from auctions.models import User, Listing, Comment, Bid
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,35 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    starting_bid = models.IntegerField()
+    image_url = models.URLField(max_length=500)
+    category = models.CharField(max_length=50)
+    listing_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name = "listings")
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE, blank = True, null = True, related_name = "listings")
+
+    def __str__(self):
+        return f"Item: {self.title}, Category: {self.category} Starting bid: {self.starting_bid}"
+
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length = 500)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "comments")
+    listing = models.ForeignKey('Listing', on_delete = models.CASCADE, related_name = "comments")
+
+    def __str__(self):
+        return f"User: {self.user}, Listing: {self.listing}, Comment: {self.comment}"
+    
+
+
+class Bid(models.Model):
+    listing = models.ForeignKey('Listing', on_delete = models.CASCADE, related_name = "bids")
+    bid = models.IntegerField()
+    
+    def __str__(self):
+        return f"Listing: {self.listing}, Bid: {self.bid}"
