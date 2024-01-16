bba2171db90a2c1dfd1ba5e7071da28d13cf35e8
Ali <ali.zolghadr@gmail.com>
2024-01-16 19:51:39 +1100

    Make image_url and category in Listing optional

--- auctions/models.py
+++ auctions/models.py
@@ -10,8 +10,8 @@ class Listing(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     starting_bid = models.IntegerField()
-    image_url = models.URLField(max_length=500)
-    category = models.CharField(max_length=50)
+    image_url = models.URLField(max_length=500, blank = True)
+    category = models.CharField(max_length=50, blank = True)
     listing_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name = "listings")
     comment = models.ForeignKey('Comment', on_delete=models.CASCADE, blank = True, null = True, related_name = "listings")
 
